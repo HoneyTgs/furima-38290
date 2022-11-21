cb73dc4b3292c6ae388f7b5ee298d44a733a6f39
HoneyTgs <112701355+HoneyTgs@users.noreply.github.com>
2022-11-21 15:03:22 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'items/index'
+  root to: 'items#index'
+  resources :orders, only:[:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
